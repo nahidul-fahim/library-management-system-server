e6d2970b2ca43d9c3e623a1a6b3d7719b2a009b1
nahidul-fahim <nahidul7562@gmail.com>
2024-11-24 11:35:12 +0600

    complete the prisma schema

---
+++ prisma/migrations/20241124053446_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateTable
+CREATE TABLE "members" (
+    "memberId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "membershipDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "members_pkey" PRIMARY KEY ("memberId")
+);
+
+-- CreateTable
+CREATE TABLE "books" (
+    "bookId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "publishedYear" INTEGER NOT NULL,
+    "totalCopies" INTEGER NOT NULL,
+    "availableCopies" INTEGER NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("bookId")
+);
+
+-- CreateTable
+CREATE TABLE "borrowRecords" (
+    "borrowId" TEXT NOT NULL,
+    "borrowDate" TIMESTAMP(3) NOT NULL,
+    "returnDate" TIMESTAMP(3),
+    "bookId" TEXT NOT NULL,
+    "memberId" TEXT NOT NULL,
+
+    CONSTRAINT "borrowRecords_pkey" PRIMARY KEY ("borrowId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "members_memberId_key" ON "members"("memberId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "members_email_key" ON "members"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "members_phone_key" ON "members"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_bookId_key" ON "books"("bookId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "borrowRecords_bookId_key" ON "borrowRecords"("bookId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "borrowRecords_memberId_key" ON "borrowRecords"("memberId");
+
+-- AddForeignKey
+ALTER TABLE "borrowRecords" ADD CONSTRAINT "borrowRecords_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "books"("bookId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "borrowRecords" ADD CONSTRAINT "borrowRecords_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "members"("memberId") ON DELETE RESTRICT ON UPDATE CASCADE;
